97e50bef2e5fae2b43ab8632d011d1adf91a0e5e
joaovitorml <joao.lemos4000@gmail.com>
2019-11-23 19:37:22 -0300

    Add knapsack codes.
    Co-authored-by: FranciscoHeronildo <heronildo.2010@hotmail.com>

---
+++ knapsack.py
@@ -0,0 +1,80 @@
+import random
+import time
+
+taken = [[0 for x in range(100) for y in range(100)]]
+
+def knapsack(pesoLim, pesos, valores, n):
+    global taken
+    i = 0
+    w = 0
+    K = [[0 for x in range(100000) for y in range(100000)]]
+    while(i<n):
+        while(w<pesoLim):
+            if(i==0 or w==0):
+                K[i][w] = 0
+            elif(pesos[i-1] <= w):
+                K[i][w] = max((valores[i-1] + K[i-1][w-pesos[i-1]]), (K[i-1][w]))
+                if(K[i][w]==valores[i-1] + K[i-1][w-pesos[i-1]]):
+                    taken[i][w] = True
+            else:
+                K[i][w] = K[i-1][w]
+            w+=1
+            print('sei la')
+        i+=1
+        print('uwu')
+
+    print("Valor da mochila = ")
+    print(K[n][pesoLim])
+    res = K[n][pesoLim]
+    w = pesoLim    
+    i = n
+
+    while(i>0 and res>0):
+        if(res == K[i - 1][w]):
+            continue
+        else:
+            print("valor do item = ", valores[i-1])
+            print("Peso = ", pesos[i-1])
+            res = res - valores[i-1]
+            w = w - pesos[i-1]
+        i-=1
+
+# memset(taken, false, sizeof taken)
+
+
+valores = []
+pesos = []
+
+print("Informe o tamanho dos vetores: ")
+size = int(input())
+
+if(size<100):
+    i=0
+    while(i<size):
+        valores.append(random.randint(0,1000))
+        i+=1
+
+if(size < 20):
+    print(valores)
+
+
+if(size<100):
+    i=0
+    while(i<size):
+        pesos.append(random.randint(0,1000))
+        i+=1
+
+if(size < 20):
+    print(pesos)
+
+print("Insira o peso máximo da mochila: ")
+pesoLim = int(input())
+
+n = len(valores)
+
+inicio = time.time()
+knapsack(pesoLim, pesos, valores, n)
+fim = time.time()
+
+print('Tempo de execucao: ', fim - inicio)
+print('\n')
